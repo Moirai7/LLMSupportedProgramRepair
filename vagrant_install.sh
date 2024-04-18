5692f2de057539ad1f92ae7a0f06a993eaaf48c0
Moirai7 <moirai.zhang@gmail.com>
2024-04-18 16:44:23 -0700

    New install

---
+++ vagrant_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.9 -y
+sudo apt-get install git-all -y
+sudo apt install python3-pip -y
+sudo apt-get install python3-distutils -y
+sudo apt-get install python3-apt -y
+sudo apt-get install build-essential
+
+make install
+python3.9 -m pip install -r requirements.txt
